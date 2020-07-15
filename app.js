17a59afccdf0d35aabfc5f91b7df56b1cce269d4
Jihoon Jang <jh145478@gmail.com>
2020-07-15 15:12:53 +0900

    add lint

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+/*global process */
+
 const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('Hello, wolrd!');
+	res.send('Hello, wolrd!');
 });
 
 app.listen(app.get('port'), () => {
-  console.log('TODO API Server listening on port ' + app.get('port'));
+	console.log('TODO API Server listening on port ' + app.get('port'));
 });
 
 module.exports = app;
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,15 +1,18 @@
-const request = require('supertest')
+/*eslint no-unused-vars: ["error", { "args": "none" }]*/
+/*global describe it*/
+
+const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-    it('정상적인 요청, 200', (done) => {
-        request(app)
-            .get('/')
-            .expect(200)
-            .end((err, res) => {
-                if (err)
-                    throw err;
-                done();
-            });
-    });
+	it('정상적인 요청, 200', (done) => {
+		request(app)
+			.get('/')
+			.expect(200)
+			.end((err, res) => {
+				if (err)
+					throw err;
+				done();
+			});
+	});
 });
