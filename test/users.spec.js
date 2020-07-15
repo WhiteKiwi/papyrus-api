7c84cbb8472408dd7b2667a7627cbfaa63812ada
Whitekiwi <s5v7@naver.com>
2020-07-16 01:57:18 +0900

    linting

--- test/users.spec.js
+++ test/users.spec.js
@@ -5,16 +5,16 @@ require('chai').should();
 describe('GET /users/', () => {
 	it('Get User By User ID', (done) => {
 		request(app)
-			.get(`/users/?id=jh145478`)
+			.get('/users/?id=jh145478')
 			.expect(200)
 			.end((err, res) => {
 				if (err)
 					throw err;
 
 				const testUser = {
-					"id": 1,
-					"user_id": "jh145478",
-					"nickname": "Kiwi"
+					'id': 1,
+					'user_id': 'jh145478',
+					'nickname': 'Kiwi'
 				};
 				res.body.id.should.be.equal(testUser.id);
 				res.body.user_id.should.be.equal(testUser.user_id);
