2f307e17d04db0a8cef4c65a1e0b9e3322a251c5
Whitekiwi <s5v7@naver.com>
2020-07-16 23:06:02 +0900

    User 관련 테스트코드 추가

--- test/users.spec.js
+++ test/users.spec.js
@@ -2,23 +2,87 @@ const request = require('supertest');
 const app = require('../app');
 require('chai').should();
 
-describe('GET /users/', () => {
-	it('Get User By User ID', (done) => {
+const testUser = {
+	'user_id': 'testUserQwerQwer',
+	'password': 'testPassword',
+	'nickname': 'TestKiwi'
+};
+
+let accessToken;
+// let refreshToken;
+
+// TODO: Test DB 분리하기
+
+describe('POST /users/', () => {
+	it('회원가입', (done) => {
 		request(app)
-			.get('/users/?id=jh145478')
+			.post('/users/')
+			.send({
+				id: testUser.user_id,
+				password: testUser.password,
+				nickname: testUser.nickname
+			})
+			.expect(201)
+			.end((err, res) => {
+				if (err)
+					throw err;
+
+				done();
+			});
+	});
+});
+
+describe('POST /users/sign-in', () => {
+	it('로그인', (done) => {
+		request(app)
+			.post('/users/sign-in')
+			.send({
+				id: testUser.user_id,
+				password: testUser.password
+			})
 			.expect(200)
 			.end((err, res) => {
 				if (err)
 					throw err;
 
-				const testUser = {
-					'id': 1,
-					'user_id': 'jh145478',
-					'nickname': 'Kiwi'
-				};
-				res.body.id.should.be.equal(testUser.id);
+				accessToken = res.body.accessToken;
+				// refreshToken = res.body.refreshToken;
+
+				done();
+			});
+	});
+});
+
+describe('GET /users/', () => {
+	it('내 정보 가져오기', (done) => {
+		request(app)
+			.get('/users/')
+			.expect(200)
+			.set({ 'Authorization': `Bearer ${accessToken}` })
+			.end((err, res) => {
+				if (err)
+					throw err;
+
 				res.body.user_id.should.be.equal(testUser.user_id);
 				res.body.nickname.should.be.equal(testUser.nickname);
+
+				done();
+			});
+	});
+});
+
+describe('DELETE /users/', () => {
+	it('회원탈퇴', (done) => {
+		request(app)
+			.delete('/users/')
+			.set({ 'Authorization': `Bearer ${accessToken}` })
+			.send({
+				password: testUser.password
+			})
+			.expect(200)
+			.end((err, res) => {
+				if (err)
+					throw err;
 
 				done();
 			});
