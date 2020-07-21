943e400065c38b398fe9cbd4903d874ac0151de3
Whitekiwi <s5v7@naver.com>
2020-07-22 00:19:34 +0900

    add api needs development

--- routes/controllers/todoController.js
+++ routes/controllers/todoController.js
@@ -1,12 +1,34 @@
 const TODO = require('../../models/todo');
 
 module.exports = {
+	// GET /todos - 유저의 Todo 리스트 반환
 	getTodos: function (req, res) {
 		TODO.getTodos(req.user).then((rows) => {
+			// TODO: 카테고리, 미해결 등 옵션 추가
 			res.json(rows);
 		}).catch((err) => { 
 			console.log(err);
 			throw err; 
 		});
+	},
+	// GET /todos/:uuid - 상세정보 반환
+	getTodoInfo: function (req, res) {
+		// TODO: API 구현
+		res.send('Comming Soon');
+	},
+	// POST /todos - Todo 추가
+	addTodo: function (req, res) {
+		// TODO: API 구현
+		res.send('Comming Soon');
+	},
+	// PATCH /todos - 정보 Update
+	updateTodo: function (req, res) {
+		// TODO: API 구현
+		res.send('Comming Soon');
+	},
+	// DELETE /todos - Todo 삭제
+	deleteTodo: function (req, res) {
+		// TODO: API 구현
+		res.send('Comming Soon');
 	}
 };
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -3,5 +3,9 @@ const todoController = require('./controllers/todoController');
 
 // TODO API
 router.get('/', todoController.getTodos);
+router.get('/:uuid', todoController.getTodoInfo);
+router.post('/', todoController.addTodo);
+router.patch('/', todoController.updateTodo);
+router.delete('/', todoController.deleteTodo);
 
 module.exports = router;
