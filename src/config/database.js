982e828c9db2d3559a0ea1f2b212daeaad074413
whitekiwi <jh145478@gmail.com>
2020-08-25 00:09:23 +0900

    DB_ADDR 네이밍 -> MYSQL_HOST

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const bluebird = require('bluebird');
 require('dotenv').config();
 
 let connectionPool = mysql.createPool({
-	host: process.env.DB_ADDR || 'localhost',
+	host: process.env.MYSQL_HOST || 'localhost',
 	user: process.env.MYSQL_ID || 'mysql',
 	password: process.env.MYSQL_PW || '',
 	database: process.env.MYSQL_DATABASE || 'todolist_dev',
